3b3e5da050f57b4a59476767d7f5fc72c3b41e99
Guénolé Guémas <guenole.guemas@gmail.com>
2011-11-27 19:10:01 +0100

    add include directory

---
+++ include/main.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string.h>
+#include <OpenKN/math/Vector.hpp>
+
+#include <OpenKN/image/Image.hpp>
+#include <OpenKN/image/ImageRGB.hpp>
+#include <OpenKN/image/ioJPG.hpp>
